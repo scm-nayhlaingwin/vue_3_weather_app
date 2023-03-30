c23025d650d5e455cf8ece864b4aa932ef627668
nayhlaingwin <nay.hlaingwin@seattleconsultingmyanmar.com>
2023-03-30 13:40:03 +0630

    delete: unnecessary code

--- src/assets/fontaAwesome.js
+++ src/assets/fontaAwesome.js
@@ -4,8 +4,6 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons';
 library.add(fas,fab);
-// import { far } from '@fortawesome/free-regular-svg-icons';
-// library.add(far);
 import { dom } from "@fortawesome/fontawesome-svg-core";
 dom.watch();
 
